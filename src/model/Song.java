a98083834be2baa121d73e50ff91fd3d13757f80
mcodepreneur <michaeldenenberg@protonmail.com>
2025-02-13 13:48:10 -0700

    song

--- src/model/Song.java
+++ src/model/Song.java
@@ -1,5 +1,27 @@
 package model;
 
 public class Song {
-
+	String name;
+	String artist;
+	int rating;
+	boolean favorite;
+	
+	public Song(String name, String artist) {
+		this.name = name;
+		this.artist = artist;
+		this.rating = 0;
+		this.favorite = false;
+	}
+	
+	public void rate(int rating) {
+		this.rating = rating;
+		if (rating == 5) {
+			favorite = true;
+		}
+	}
+	
+	public void favorite(boolean favorite) {
+		this.favorite = favorite;
+	}
+	
 }
