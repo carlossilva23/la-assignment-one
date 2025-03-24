2daa4ec0e306c0c41864476e4936b76f862230d5
carlossilva23 <carlossilva23@arizona.edu>
2025-03-23 20:33:01 -0700

    Updating Main

--- src/model/User.java
+++ src/model/User.java
@@ -4,29 +4,29 @@ public class User {
 	private String username;
 	private String passwordHash;
 	private LibraryModel userLibrary;
-	
-	// For creating new users
+
+	// For Creating New Users
 	public User(String username, String passwordHash) {
 		this.username = username;
 		this.passwordHash = passwordHash;
 		this.userLibrary = new LibraryModel();
 	}
-	
-	// For loading users
+
+	// For Loading Users
 	public User(String username, String passwordHash, LibraryModel existingLibrary) {
 		this.username = username;
 		this.passwordHash = passwordHash;
 		this.userLibrary = existingLibrary;
 	}
-	
+
 	public String getUsername() {
 		return username;
 	}
-	
+
 	public String getPasswordHash() {
 		return passwordHash;
 	}
-	
+
 	public LibraryModel getLibrary() {
 		return userLibrary;
 	}
